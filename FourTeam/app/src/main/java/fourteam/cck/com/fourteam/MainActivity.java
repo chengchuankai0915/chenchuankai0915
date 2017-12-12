1ffb7f3bec73f8fdb982d65b83116ed38bc577e7
chengchuankai <1227357282@qq.com>
2017-12-12 20:06:39 +0800

    cck

---
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/MainActivity.java
@@ -0,0 +1,13 @@
+package fourteam.cck.com.fourteam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
