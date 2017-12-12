57b49daa4181264b9e1ecd0905abbad8f5b4a8b9
chengchuankai <1227357282@qq.com>
2017-12-12 21:34:53 +0800

    cck0

--- FourTeam/app/src/main/java/fourteam/cck/com/fourteam/MainActivity.java
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //注解
+        //第二次
     }
 }
