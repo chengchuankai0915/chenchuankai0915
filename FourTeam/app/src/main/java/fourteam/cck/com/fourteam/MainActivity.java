62b5ab0bec4c3749cb6053303491f3135d9e225f
chengchuankai <1227357282@qq.com>
2017-12-12 20:25:31 +0800

    aaa

--- FourTeam/app/src/main/java/fourteam/cck/com/fourteam/MainActivity.java
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/MainActivity.java
@@ -3,8 +3,7 @@ package fourteam.cck.com.fourteam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
