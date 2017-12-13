13df7429ee4d7b23cf96061bf4a76d4ea8461657
chengchuankai <1227357282@qq.com>
2017-12-13 14:17:24 +0800

    add buju2

---
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/fragment/SpecialFragment.java
@@ -0,0 +1,23 @@
+package fourteam.cck.com.fourteam.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fourteam.cck.com.fourteam.R;
+
+/**
+ * Created by C-PC on 2017/12/13.
+ */
+
+public class SpecialFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.special_fragment, null);
+        return view;
+    }
+}
