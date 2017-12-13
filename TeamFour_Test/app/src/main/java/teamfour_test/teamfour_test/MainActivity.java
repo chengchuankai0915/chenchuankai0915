70cd884680aaa4c8deee0a7fb3620727ab3022b2
androidQAQ <1023078708@qq.com>
2017-12-13 20:50:01 +0800

    Test

---
+++ TeamFour_Test/app/src/main/java/teamfour_test/teamfour_test/MainActivity.java
@@ -0,0 +1,16 @@
+package teamfour_test.teamfour_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
