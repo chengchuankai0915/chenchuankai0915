ebe1aad577c7bbfb856fe25bb529b9109f29baef
androidQAQ <1023078708@qq.com>
2017-12-14 18:39:23 +0800

    csh1214

--- FourTeam/app/src/main/java/fourteam/cck/com/fourteam/fragment/SpecialFragment.java
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/fragment/SpecialFragment.java
@@ -3,18 +3,22 @@ package fourteam.cck.com.fourteam.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import fourteam.cck.com.fourteam.R;
+import fourteam.cck.com.fourteam.bean.ChoicenessBean;
+import fourteam.cck.com.fourteam.cck.presenter.ChoicenessPresenterImpl;
+import fourteam.cck.com.fourteam.cck.view.ChoicenessView;
 
 /**
  * Created by C-PC on 2017/12/13.
  */
 
-public class SpecialFragment extends Fragment{
+public class SpecialFragment extends Fragment implements ChoicenessView{
 
     private RecyclerView recyc_special;
 
@@ -24,9 +28,20 @@ public class SpecialFragment extends Fragment{
         View view = inflater.inflate(R.layout.special_fragment, null);
         //找控件
         recyc_special = view.findViewById(R.id.recyc_special);
+        //
+        ChoicenessPresenterImpl choicenessPresenter=new ChoicenessPresenterImpl(this);
+        choicenessPresenter.guanlian();
 
+        recyc_special.setLayoutManager(new GridLayoutManager(getActivity(),2));
 
 
         return view;
+    }
+
+    @Override
+    public void showData(ChoicenessBean choicenessBean) {
+
+
+
     }
 }
