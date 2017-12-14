8a2316cfa39fc85194b5a6ff692460c631cdb412
androidQAQ <1023078708@qq.com>
2017-12-14 08:17:57 +0800

    1214

---
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/csh/model/SpecialModel.java
@@ -0,0 +1,8 @@
+package fourteam.cck.com.fourteam.csh.model;
+
+/**
+ * Created by 曹少航 on 2017/12/13.
+ */
+
+public interface SpecialModel {
+}
---
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/csh/view/SpecialView.java
@@ -0,0 +1,9 @@
+package fourteam.cck.com.fourteam.csh.view;
+
+/**
+ * Created by 曹少航 on 2017/12/13.
+ */
+
+public interface SpecialView {
+
+}
