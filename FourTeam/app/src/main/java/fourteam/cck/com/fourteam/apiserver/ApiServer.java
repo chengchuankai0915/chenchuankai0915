8c5a0f89c577e603d9d740a8312fa4081bcb86c3
chengchuankai <1227357282@qq.com>
2017-12-15 17:18:46 +0800

    1215

--- FourTeam/app/src/main/java/fourteam/cck/com/fourteam/apiserver/ApiServer.java
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/apiserver/ApiServer.java
@@ -1,11 +1,11 @@
 package fourteam.cck.com.fourteam.apiserver;
 
 import fourteam.cck.com.fourteam.bean.ChoicenessBean;
-<<<<<<< HEAD
+
 import fourteam.cck.com.fourteam.bean.FaXianBean;
-=======
+
 import fourteam.cck.com.fourteam.bean.CommentBean;
->>>>>>> aa5864b399f830de74f38ed4f7b6874d1189254b
+
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import rx.Observable;
@@ -24,13 +24,13 @@ public interface ApiServer {
 
     @GET("homePageApi/homePage.do")
     Observable<ChoicenessBean> getParms();
-<<<<<<< HEAD
+
 /**
  * 发现界面
  */
     @GET("columns/getVideoList.do")
     Observable<FaXianBean>    getFind(@Query("catalogId") String catalogId , @Query("pnum") String pnum);
-=======
+
 
     /**
      * 评论http://api.svipmovie.com/front/Commentary/getCommentList.do?mediaId=2b6ae30e59af48fb8a8cbec28535f3cd
@@ -38,5 +38,5 @@ public interface ApiServer {
 
     @GET("Commentary/getCommentList.do")
     Observable<CommentBean> getHasParms(@Query("mediaId") String mediaId);
->>>>>>> aa5864b399f830de74f38ed4f7b6874d1189254b
+
 }
