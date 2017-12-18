818bc4a7bda1b21679d20b5752890f6c27a168a5
chengchuankai <1227357282@qq.com>
2017-12-18 14:08:37 +0800

    cck1218

--- FourTeam/app/src/main/java/fourteam/cck/com/fourteam/activity/LishiActivity.java
+++ FourTeam/app/src/main/java/fourteam/cck/com/fourteam/activity/LishiActivity.java
@@ -1,7 +1,11 @@
 package fourteam.cck.com.fourteam.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import fourteam.cck.com.fourteam.R;
 
@@ -11,5 +15,16 @@ public class LishiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lishi);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
